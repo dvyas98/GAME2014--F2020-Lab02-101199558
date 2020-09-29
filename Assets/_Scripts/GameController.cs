c1fefa09b7be0a211b025dc47841ac4a4f0a38c5
dvyas98 <dhimant92@gmail.com>
2020-09-29 11:31:05 -0400

    first commit

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class GameController : MonoBehaviour
+{
+   public TMP_Text Score,Lives;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (Input.deviceOrientation)
+        {
+            case DeviceOrientation.LandscapeLeft:
+                Lives.rectTransform.anchoredPosition = new Vector3(370.0f, -105.0f, 0.0f);
+                Score.rectTransform.anchoredPosition = new Vector3(-349.0f, -105.0f, 0.0f);
+                //SceneLable.text= "Left";
+                break;
+            case DeviceOrientation.LandscapeRight:
+                Lives.rectTransform.anchoredPosition = new Vector3(370.0f, -105.0f, 0.0f);
+                Score.rectTransform.anchoredPosition = new Vector3(-349.0f, -105.0f, 0.0f);
+                //SceneLable.text = "Right";
+                break;
+            case DeviceOrientation.Portrait:
+               // SceneLable.text = "Portrait";
+                break;
+            case DeviceOrientation.Unknown:
+                Debug.Log("Unknown Orientation");
+                break;
+        }
+    }
+}
+
---
+++ Assets/_Scripts/TestButtonBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TestButtonBehaviour : MonoBehaviour
+{
+    public TMP_Text SceneLable,Score,Lives;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTestButtonPressed()
+    {
+        Debug.Log("TestButton Pressed");
+        SceneLable.text = "TEST";
+        Lives.rectTransform.anchoredPosition = new Vector3(370.0f, -105.0f, 0.0f);
+        Score.rectTransform.anchoredPosition = new Vector3(-349.0f, -105.0f, 0.0f);
+    }
+}
